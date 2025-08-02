c15a7b82eb1eb4f1d41d30e3059e0269ecc3437a
harish <muthumuruganharish272003@gmail.com>
2025-08-02 16:58:49 +0530

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react' 
+import React from 'react'
 import Home from './pages/Home'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 const App = () => {
   return (
-    <div>
-      <Home/>
-      
-    </div>
+    <>
+
+      <BrowserRouter>
+        <Routes>
+
+          <Route path="/" element={<Home />} />
+
+        </Routes>
+        
+
+      </BrowserRouter>
+
+    </>
   )
 }
 
