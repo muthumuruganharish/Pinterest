115159ba617db2efbe27713ce7bda01b944f0276
Hemanath_Kumar <99248097+Hemanath-Kumar@users.noreply.github.com>
2025-08-02 20:30:58 +0530

    merge Navbar & Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import { useState } from 'react'
-import Sidebar from './components/Sidebar/SIdebar'
 import React from 'react'
 import Home from './pages/Home'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
 
       <BrowserRouter>
         <Routes>
-          <Sidebar/>
+          
           <Route path="/" element={<Home />} />
 
         </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
+import Sidebar from '../components/Sidebar/Sidebar'
+import "../Styles/Home.css"
 
 const Home = () => {
   return (
     <>
-      <Navbar/>
+      <div className="main">
+          <Sidebar/>
+
+        <div className="two">
+          <div className="nav">
+              <Navbar/>
+          </div>
+          <div className="content">
+            asd
+          </div>
+              
+        </div>
+      </div>
+      
+      
     </>
   )
 }
